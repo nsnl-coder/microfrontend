4df07f73e966d161cf9ed92d99d87c5bc1f140b3
nsnhatlong <nsnl.coder@gmail.com>
2023-01-14 05:00:05 -0500

    create container deploy script

--- 1.container/config/webpack.dev.js
+++ 1.container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const commonConfig = require("./webpack.common");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
@@ -14,15 +13,12 @@ const devConfig = {
     },
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
         marketing: "marketing@http://localhost:8081/remoteEntry.js",
       },
-      shared: ["react", "react-dom", "react-router-dom"],
+      shared: packageJson.dependencies,
     }),
   ],
 };
